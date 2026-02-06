1fc1c7c2ca6bc02eeff963567adc18fa24fcf623
Moe Naser <moe.b.naser@gmail.com>
2026-02-06 19:58:41 +0000

    initial commit

--- alembic/versions/38b09a2cb7e1_add_content_column_to_posts_table.py
+++ alembic/versions/38b09a2cb7e1_add_content_column_to_posts_table.py
@@ -19,7 +19,7 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.add_column("posts", sa.Column('content', sa.String, nullable= False))
+    op.add_column("posts", sa.Column('content', sa.String(), nullable= False))
     pass
 
 
